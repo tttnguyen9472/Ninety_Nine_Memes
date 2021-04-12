bf1931c0c8539acd6c94f09d93e54a4b98f0482c
Vona Csaba <vcsneu@gmail.com>
2021-04-12 13:13:54 +0200

    general fetch

---
+++ frontend/src/components/fetch/Fetch.js
@@ -0,0 +1,28 @@
+async function Fetch(method, endpoint, body) {
+  const settings = {
+    method: method,
+    headers: { 'Content-Type': 'application/json' },
+  };
+
+  settings.headers.token = JSON.stringify(
+    JSON.parse(atob(localStorage.getItem('token').split('.')[1]))
+  );
+
+  if (body) {
+    settings.body = JSON.stringify(body);
+  }
+
+  const call = await fetch(
+    `${process.env.REACT_APP_PORT}${endpoint}`,
+    settings
+  );
+
+  const result = await call.json();
+
+  if (call.status !== 200) {
+    throw new Error(result);
+  }
+  return result;
+}
+
+export default Fetch;
