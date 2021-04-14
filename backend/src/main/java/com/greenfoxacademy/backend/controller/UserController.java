425fec6f6d89a419bd9515d92383a315e3f5fd85
Babinszky Daniel <62801558+daanworks@users.noreply.github.com>
2021-04-14 17:06:10 +0200

    added cors (#9)

--- backend/src/main/java/com/greenfoxacademy/backend/controller/UserController.java
+++ backend/src/main/java/com/greenfoxacademy/backend/controller/UserController.java
@@ -9,6 +9,7 @@ import com.greenfoxacademy.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
