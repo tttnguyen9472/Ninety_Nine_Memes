f1eccd1121b11eebff43d3d1f3b409886b73edd2
daanworks <daniel.babinszky@gmail.com>
2021-04-14 17:04:39 +0200

    added cors

--- backend/src/main/java/com/greenfoxacademy/backend/controller/MemeController.java
+++ backend/src/main/java/com/greenfoxacademy/backend/controller/MemeController.java
@@ -17,11 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
