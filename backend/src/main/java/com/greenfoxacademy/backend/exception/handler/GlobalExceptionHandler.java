16d6e5012fd874502f24680e120aee64b2bb2117
Peter Toth <t.peter90@gmail.com>
2021-04-15 11:04:28 +0200

    logger, HttpMessageNotReadableException handling

--- backend/src/main/java/com/greenfoxacademy/backend/exception/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/greenfoxacademy/backend/exception/handler/GlobalExceptionHandler.java
@@ -6,12 +6,15 @@ import com.greenfoxacademy.backend.exception.reactionException.InvalidReactionVa
 import com.greenfoxacademy.backend.exception.memeException.MissingParameterException;
 import com.greenfoxacademy.backend.exception.reactionException.NoSuchReactionException;
 import com.greenfoxacademy.backend.exception.userException.ReservedUsernameException;
+import com.greenfoxacademy.backend.model.ErrorDTO;
 import com.greenfoxacademy.backend.model.meme.MemeErrorDTO;
 import com.greenfoxacademy.backend.model.reaction.ReactionErrorDTO;
 import com.greenfoxacademy.backend.model.user.RegisterResponseDTO;
 import com.greenfoxacademy.backend.model.user.UserLoginDTO;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,43 +23,61 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+  private static final Logger logger = Logger.getLogger(GlobalExceptionHandler.class);
+
   @ExceptionHandler(ReservedUsernameException.class)
   public ResponseEntity<RegisterResponseDTO> reservedUsernameExceptionHandling(ReservedUsernameException ex) {
+    logger.warn("ReservedUsernameException: " + ex.getRequestedName());
     return new ResponseEntity<>(new RegisterResponseDTO("Username already taken, please choose an other one."),
         HttpStatus.CONFLICT);
   }
 
   @ExceptionHandler(MissingParameterException.class)
   public ResponseEntity<RegisterResponseDTO> missingParameterExceptionHandling(MissingParameterException ex) {
+    logger.warn("MissingParameterException: " + ex.getMissingParameterList());
     String message = "Missing parameter(s): " + String.join(", ", ex.getMissingParameterList());
     return new ResponseEntity<>(new RegisterResponseDTO(message), HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(UsernameNotFoundException.class)
   public ResponseEntity<UserLoginDTO> usernameNotFoundExceptionHandling() {
+    logger.warn("UsernameNotFoundException");
     return new ResponseEntity<>(new UserLoginDTO("No such user can be found!"), HttpStatus.UNAUTHORIZED);
   }
 
   @ExceptionHandler(BadCredentialsException.class)
   public ResponseEntity<UserLoginDTO> badCredentialsExceptionHandling() {
+    logger.warn("BadCredentialsException - wrong password");
     return new ResponseEntity<>(new UserLoginDTO("Wrong password!"), HttpStatus.UNAUTHORIZED);
   }
 
   @ExceptionHandler(InvalidMemeIdException.class)
   public ResponseEntity<MemeErrorDTO> invalidMemeIdExceptionHandling(InvalidMemeIdException ex) {
+    logger.warn("InvalidMemeIdException - the specified Meme ID is invalid");
         return new ResponseEntity<>(new MemeErrorDTO("The specified Meme ID is invalid."),
         HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(NoSuchReactionException.class)
   public ResponseEntity<ReactionErrorDTO> noSuchReactionExceptionHandling(NoSuchReactionException ex) {
+    logger.warn("NoSuchReactionException - you can't react like that bro");
     return new ResponseEntity<>(new ReactionErrorDTO("You can't react like that bro."),
         HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(InvalidReactionValueException.class)
   public ResponseEntity<ReactionErrorDTO> invalidReactionValueExceptionHandling(InvalidReactionValueException ex) {
+    logger.warn("InvalidReactionValueException - you can't react like that bro");
     return new ResponseEntity<>(new ReactionErrorDTO("You can't react like that bro."),
         HttpStatus.BAD_REQUEST);
   }
+
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<ErrorDTO> httpMessageNotReadableExceptionHandling(HttpMessageNotReadableException ex) {
+    logger.warn("HttpMessageNotReadableException - the message is not readable");
+    return new ResponseEntity<>(new ErrorDTO("The message is not readable."),
+        HttpStatus.BAD_REQUEST);
+  }
+
 }
--- backend/src/main/java/com/greenfoxacademy/backend/exception/handler/TokenExceptionHandler.java
+++ backend/src/main/java/com/greenfoxacademy/backend/exception/handler/TokenExceptionHandler.java
@@ -2,6 +2,7 @@ package com.greenfoxacademy.backend.exception.handler;
 
 import com.greenfoxacademy.backend.security.ErrorEntityDTO;
 import io.jsonwebtoken.JwtException;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class TokenExceptionHandler {
 
+  private static final Logger logger = Logger.getLogger(TokenExceptionHandler.class);
+
   @ExceptionHandler(JwtException.class)
   @ResponseStatus(HttpStatus.UNAUTHORIZED)
   public ErrorEntityDTO processJwtException(JwtException ex) {
+    logger.error(ex.getMessage());
+    logger.debug(ex);
     return new ErrorEntityDTO(HttpStatus.UNAUTHORIZED, "Unauthorized request.");
   }
 
@@ -20,8 +25,12 @@ public class TokenExceptionHandler {
   @ResponseStatus(HttpStatus.UNAUTHORIZED)
   public ErrorEntityDTO processIllegalArgumentException(IllegalArgumentException ex) {
     if (ex.getMessage().equals("JWT String argument cannot be null or empty.")) {
+      logger.error(ex.getMessage());
+      logger.debug(ex);
       return new ErrorEntityDTO(HttpStatus.UNAUTHORIZED, "JWT String argument cannot be null or empty.");
     }
+    logger.warn(ex.getMessage());
+    logger.debug(ex);
     throw ex;
   }
 
