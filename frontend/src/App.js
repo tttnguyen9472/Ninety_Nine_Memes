136098817b42571d9a94448ab0d7b74451bdc3f2
Martin Smitnya <smitnyamartin@gmail.com>
2021-04-15 11:12:30 +0200

    merging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/login/FormStyled'
-import FormStyled from './components/login/FormStyled';
-import MemeForm from './components/memeCreator/MemeForm';
-import MemeCreator from './components/memeCreator/MemeCreator';
-import Gallery from './components/memeCreator/Gallery'
 
 function App() {
   return (
     <div className="App">
-      {/* <UploadFile /> */}
-      {/* <MemeForm /> */}
-      {/* <MemeCreator /> */}
-      <FormStyled/>
+    <Login />
     </div>
   );
 }
